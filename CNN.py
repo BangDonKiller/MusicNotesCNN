2659ed00bfb4551dcd01be04b09e5001c7850b0c
Ricepaste <n22126@gmail.com>
2024-05-30 03:07:20 +0800

    can training now

--- CNN.py
+++ CNN.py
@@ -1,55 +1,49 @@
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-from torch.utils.data import DataLoader
-
 import pandas as pd
 import numpy as np
 
+import Model
+import MusicData
 
-df = pd.read_csv('./dataset.csv')
-train_data = df.drop(columns=['label']).values
-train_data_label = df['label'].values
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("device: ", device)
 
-# 計算Train資料中的音符類別及數量
-unique, counts = np.unique(train_data_label, return_counts=True)
-print("Train dataset note amount: ", dict(zip(unique, counts)))
+model = Model.CNN().to(device)
+criterion = torch.nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-df2 = pd.read_csv('./data1.csv')
-test_data = df2.drop(columns=['label']).values
-test_data_label = df2['label'].values
+train_loader = MusicData.train_loader
+test_loader = MusicData.test_loader
 
-# 計算Test資料中的音符類別及數量
-unique, counts = np.unique(test_data_label, return_counts=True)
-print("Test dataset note amount: ", dict(zip(unique, counts)))
+for epoch in range(10):
+    model.train()
+    for i, (data, label) in enumerate(train_loader):
+        # print data shape
+        # print(data.shape)
 
-def one_hot_encode(labels):
+        data, label = data.to(device), label.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, label)
 
+        loss.backward()
+        optimizer.step()
+        if i % 100 == 0:
+            print(f"Epoch: {epoch}, Loss: {loss.detach().cpu().item()}")
 
-
-print("Before one-hot encoding: ", train_data_label)
-print("After one-hot encoding: ", one_hot_encode(train_data_label))
-
-class CNN(nn.Module):
-    def __init__(self):
-        super(CNN, self).__init__()
-        self.conv1 = nn.Conv2d(28, 50, 3)
-        self.conv2 = nn.Conv2d(50, 100, 3)
-        self.conv3 = nn.Conv2d(100, 200, 3)
-        self.fc1 = nn.Linear(200 * 1 * 1, 100)
-        self.fc2 = nn.Linear(100, 50)
-        self.fc3 = nn.Linear(50, 12)
-        
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = F.relu(self.conv3(x))
-        x = F.max_pool2d(x, 2, 2)
-        x = x.view(-1, 200 * 1 * 1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
-        return F.log_softmax(x, dim=1)
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for data, label in test_loader:
+            data, label = data.to(device), label.to(device)
+            output = model(data)
+            proba, predicted = torch.max(output, 1)
+            total += label.size(0)
+            correct += (predicted == label).sum().item()
+            # if predicted == label:
+            #     print(
+            #         f"probabilities: {proba}, predicted: {predicted}, actual: {label}"
+            #     )
+        print(f"Epoch: {epoch}, Accuracy: {100 * correct / total}")
---
+++ MusicData.py
@@ -0,0 +1,54 @@
+from torch.utils.data import DataLoader
+from torch.utils.data import Dataset
+import pandas as pd
+import numpy as np
+from torchvision import transforms
+import torch
+
+
+class MusicDataset(Dataset):
+
+    def __init__(self, csv_file, transform=None):
+        self.data = pd.read_csv(csv_file)
+        self.transform = transform
+        self.labels = {
+            label: idx
+            for idx, label in enumerate(sorted(self.data['label'].unique()))
+        }
+        self.img_data = self.data.drop(columns=['label']).values.astype(
+            np.float32)
+        self.img_labels = self.data['label'].map(self.labels).values.astype(
+            np.int64)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        if (self.img_data[idx].shape[0] == 784):
+            img = self.img_data[idx].reshape((28, 28))
+        else:
+            img = self.img_data[idx].reshape((64, 64))
+        label = self.img_labels[idx]
+
+        if self.transform:
+            img = self.transform(img)
+        else:
+            img = torch.tensor(img, dtype=torch.float32).unsqueeze(0)
+
+        return img, label
+
+
+# 確保 transform 處理正確
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((28, 28)),
+    transforms.ToTensor()
+])
+
+# loader 版本
+train_loader = DataLoader(MusicDataset('./dataset.csv', transform=transform),
+                          batch_size=10,
+                          shuffle=True)
+test_loader = DataLoader(MusicDataset('./data1.csv', transform=transform),
+                         batch_size=10,
+                         shuffle=False)
