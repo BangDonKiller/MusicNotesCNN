32146807e3f7c13719be12f184f96bae93f290fa
Paris Shieh <parisdata@gmail.com>
2024-06-29 17:05:43 +0800

    finishing the preprocessing and add readme

--- preprocessing.py
+++ preprocessing.py
@@ -1,24 +1,17 @@
-# download musicHub from https://musescore.org/zh-hant/download then install musicScore
-
-from music21 import *
-env = environment.Environment()
-    
-environment.Environment()['musicxmlPath'] = r'C:\Program Files\MuseScore 4\bin\MuseScore4.exe'
-environment.Environment()['musescoreDirectPNGPath'] = r'C:\Program Files\MuseScore 4\bin\MuseScore4.exe'
-
-print('musicxmlPath:', environment.Environment()['musicxmlPath'])
-print('musescoreDirectPNGPath:', environment.Environment()['musescoreDirectPNGPath'])
-
-# -------------------------------------------
-
 import os 
 import music21 as m21
+import json
+from tensorflow import keras
+import numpy as np
 
 KERN_DATASET_PATH = 'datasets/deutschl/test'
 ACCEPTABLE_DURATIONS = [
     0.25, 0.5, 0.75, 1.0, 1.5, 2, 3, 4
 ]
 SAVE_DIR = 'Dataset'
+SINGLE_FILE_DATASET = 'Dataset/file_dataset'
+SEQUENCE_LENGTH = 64
+MAPPING_PATH = 'Dataset/mapping.json'
 
 def load_songs_in_kern(data_path):
     songs = []
@@ -103,13 +96,94 @@ def preprocessing(data_path):
         save_path = os.path.join(SAVE_DIR, str(i))
         with open(save_path, "w") as file:
             file.write(encoded_song)
+            
+def load(file_path):
+    with open(file_path, "r") as file:
+        song = file.read()
+    return song
+            
+def create_single_file_dataset(dataset_path, file_dataset_path, sequence_length):
+    new_song_delimiter = "/ " * sequence_length
+    songs = ""
+    
+    # load encoded songs and add delimiters
+    for path, _, files in os.walk(dataset_path):
+        for file in files:
+            file_path = os.path.join(path, file)
+            song = load(file_path)
+            songs = songs + song + " " + new_song_delimiter
+
+    songs = songs[:-1]
+    
+    # save string that contains all dataset
+    if not os.path.exists(os.path.split(file_dataset_path)[0]):
+        with open(file_dataset_path, "w") as f:
+            f.write(songs)
+        
+    return songs
+
+def create_mapping(songs, mapping_path):
+    mappings = {}
+    
+    # identify the vocabulary
+    songs = songs.split()
+    vocabulary = list(set(songs))
+    
+    # create mappings
+    for i, symbol in enumerate(vocabulary):
+        mappings[symbol] = i
+        
+    # save vocabulary to a json file
+    if not os.path.exists(os.path.split(mapping_path)[0]):
+        with open(mapping_path, "w") as f:
+            json.dump(mappings, f, indent=4)
+        
+def convert_songs_to_int(songs):
+    int_songs = []
+    
+    # load mappings
+    with open(MAPPING_PATH, "r") as f:
+        mappings = json.load(f)
+        
+    # cast songs string to a list
+    songs = songs.split()
+    
+    # map songs to int
+    for symbol in songs:
+        int_songs.append(mappings[symbol])
+        
+    return int_songs
+
+def generate_training_sequences(sequence_length):
+    # if input length is 2
+    # [11, 12, 13, 14, ...] -> input: [11, 12], target:13; input: [12, 13], target: 14
+    
+    # load songs and map them to int
+    songs = load(SINGLE_FILE_DATASET)
+    int_songs = convert_songs_to_int(songs)
+    
+    # generate the training sequences
+    # 100 symbols, 64 sequence length -> 100 - 64 = 36 input
+    inputs = []
+    targets = []
+    
+    num_sequences = len(int_songs) - sequence_length
+    for i in range(num_sequences):
+        inputs.append(int_songs[i:i+sequence_length])
+        targets.append(int_songs[i+sequence_length])
+    
+    # one-hot encode the sequences
+    # inputs shape: (num_sequences, sequence_length) -> (num_sequences, sequence_length, vocabulary_size)
+    # [11, 12, 13] -> [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+    vocabulary_size = len(set(int_songs))
+    inputs = keras.utils.to_categorical(inputs, num_classes=vocabulary_size)
+    targets = np.array(targets)
+    
+    return inputs, targets
     
 if __name__ == '__main__':
-    songs = load_songs_in_kern(KERN_DATASET_PATH)
-    song = songs[0]
-    
     preprocessing(KERN_DATASET_PATH)
+    songs = create_single_file_dataset(SAVE_DIR, SINGLE_FILE_DATASET, SEQUENCE_LENGTH)
+    create_mapping(songs, MAPPING_PATH)
+    inputs, targets = generate_training_sequences(SEQUENCE_LENGTH)
     
-    
-    transposed_song = transpose(song)
-    transposed_song.show()
---
+++ readme.md
@@ -0,0 +1,13 @@
+If you want to see the notes in the staff, please do the following steps
+# download musicHub from https://musescore.org/zh-hant/download, then install musicScore4
+
+## type this code in your terminal:
+from music21 import *
+env = environment.Environment()
+    
+environment.Environment()['musicxmlPath'] = r'C:\Program Files\MuseScore 4\bin\MuseScore4.exe'
+environment.Environment()['musescoreDirectPNGPath'] = r'C:\Program Files\MuseScore 4\bin\MuseScore4.exe'
+
+print('musicxmlPath:', environment.Environment()['musicxmlPath'])
+print('musescoreDirectPNGPath:', environment.Environment()['musescoreDirectPNGPath'])
+------------------------------------------------
